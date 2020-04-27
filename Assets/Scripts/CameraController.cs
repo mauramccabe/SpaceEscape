9d74126b381519681111444b8f37b3672694850c
Ben <Benverney@yahoo.com>
2020-04-27 15:36:11 -0700

    player movement + initial level design

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    void Start()
+    {
+        offset =  player.transform.position - transform.position;
+        
+        
+        
+        //place pivot on top of player
+        pivot.transform.position = player.transform.position;
+        //make pivot a child of player
+        pivot.transform.parent = player.transform;
+
+        //lock mouse
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void LateUpdate()
+    {
+        //get x position of mouse and rotate the player
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        player.transform.Rotate(0, horizontal, 0);
+
+
+        //get y position of mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.transform.Rotate(-vertical, 0, 0);
+
+        float desiredYAgngle = player.transform.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAgngle, 0);
+        transform.position = player.transform.position - (rotation * offset);
+
+
+        //prevent camera clipping through floor
+
+        if(transform.position.y < player.transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.transform.position.y - .5f, transform.position.z);
+        }
+        transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+    public float gravityScale;
+    private Vector3 moveDirection;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        //normalize movedirection so you dont get double speed when moving diagonally.
+        moveDirection = Vector3.ClampMagnitude(moveDirection,1) * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y  * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
