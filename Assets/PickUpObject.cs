f1c6d07e23f0012e28fc38147b8e89756f212771
Maura McCabe <mauramccabe@Mauras-MacBook-Air.local>
2020-04-28 00:27:41 -0700

    Added picking up boxes

---
+++ Assets/PickUpObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObject : MonoBehaviour {
+	public Transform player;
+
+	bool hasPlayer = false;
+	bool beingCarried = false;
+	Rigidbody rb;
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		rb = GetComponent<Rigidbody>();
+		print("test");
+		hasPlayer = true;
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		hasPlayer = false;
+	}	
+	
+	void Update()
+	{
+		if(beingCarried)
+		{
+			if(Input.GetKeyDown("k"))
+			{
+				rb.isKinematic = false;
+				transform.parent = null;
+				beingCarried = false;
+
+			}
+		}
+		else
+		{
+			if(Input.GetKeyDown("k") && hasPlayer)
+			{
+				rb.isKinematic = true;
+				transform.parent = player;
+				beingCarried = true;
+			}
+		}
+	}
+}
