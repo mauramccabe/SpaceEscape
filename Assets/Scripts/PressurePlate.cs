39835b1bf3922882d8cf5aab631c92d88f6cd124
Olivia Pannell <livvieolive@gmail.com>
2020-04-28 18:12:32 -0700

    single pressure plate trigger working

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    void OnTriggerEnter(Collider col)
+    {
+        door.transform.position = door.transform.position + new Vector3(0, -4, 0);
+    }
+    void OnTriggerExit(Collider col)
+    {
+        door.transform.position = door.transform.position + new Vector3(0, 4, 0);
+    }
+}
