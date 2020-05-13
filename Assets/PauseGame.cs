3643adee3d43c669eb8307fd05a5221308ddd5e2
Maura McCabe <mauramccabe@Mauras-MacBook-Air.local>
2020-05-13 10:08:32 -0700

    added pause button

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+ void Update()
+ {
+     if (Input.GetKeyDown(KeyCode.P))
+     {
+         if (Time.timeScale == 1)
+         {
+             Time.timeScale = 0;
+         }
+         else
+         {
+             Time.timeScale = 1;
+         }
+     }
+ }
+}
