3a759ae2e44005432c6ad8fc35cbed470fb0a666
Olivia Pannell <livvieolive@gmail.com>
2020-05-25 11:17:37 -0700

    intro room of first level finished

---
+++ Assets/Scripts/FirstPressurePlate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPressurePlate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    void OnTriggerEnter(Collider col)
+    {
+        door.transform.position = door.transform.position + new Vector3(0, 5, 0);
+    }
+    void OnTriggerExit(Collider col)
+    {
+        door.transform.position = door.transform.position + new Vector3(0, -5, 0);
+    }
+}
