a431f33e8dbed42f198f1cfb8da510829d532e73
Maura McCabe <mauramccabe@Mauras-MacBook-Air.local>
2020-05-26 13:40:22 -0700

    mars update

---
+++ Assets/Scripts/FourPressurePlate.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourPressurePlate : MonoBehaviour
+{
+   
+
+	[SerializeField]
+    GameObject door;
+    public int pressure = 0;
+    public float height;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Box")
+        {
+            
+            pressure = pressure + 1;
+            
+        }
+        if (other.gameObject.tag == "Player")
+        {         
+            pressure = pressure + 4;
+            
+        }
+        if(pressure == 4)
+        {
+        	door.transform.position = door.transform.position + new Vector3(0, -height, 0);
+        }
+
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Box")
+        {
+            
+            pressure = pressure - 1;
+          
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            
+            pressure = pressure - 4;
+            
+        }
+        if(pressure < 4)
+        {
+        	door.transform.position = door.transform.position + new Vector3(0, height, 0);
+
+        }
+
+
+    }
+    void update()
+    {
+    	if(pressure == 4)
+    	{
+    		door.transform.position = door.transform.position + new Vector3(0, -height, 0);
+    	}
+    	else
+    	{
+    		door.transform.position = door.transform.position + new Vector3(0, height, 0);
+    	}
+    }
+}
---
+++ Assets/Scripts/PressurePlateMars.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateMars : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    public float height;
+
+    void OnTriggerEnter(Collider col)
+    {
+        door.transform.position = door.transform.position + new Vector3(0, -height, 0);
+    }
+    void OnTriggerExit(Collider col)
+    {
+        door.transform.position = door.transform.position + new Vector3(0, height, 0);
+    }
+}
