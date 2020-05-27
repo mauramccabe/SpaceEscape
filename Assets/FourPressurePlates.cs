56630976c979b42f14e4b39c1a0a844f42e3698e
Maura McCabe <mauramccabe@Mauras-MacBook-Air.local>
2020-05-26 17:24:20 -0700

    adding scripts

---
+++ Assets/FourPressurePlates.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourPressurePlates : MonoBehaviour
+{
+   
+    public int pressure = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Box")
+        {
+            
+            pressure = pressure + 1;
+            
+        }
+        if (other.gameObject.tag == "Player")
+        {         
+            pressure = pressure + 4;
+            
+        }
+
+
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Box")
+        {
+            
+            pressure = pressure - 1;
+          
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            
+            pressure = pressure - 4;
+            
+        }
+
+
+    }
+}
