516aa494a1d11019a8cc9f8c540aa37167945da5
Olivia Pannell <livvieolive@gmail.com>
2020-05-27 00:21:53 -0700

    tutorial cave

---
+++ Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        int sc = SceneManager.GetActiveScene().buildIndex;
+        if (sc == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(sc + 1);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject txt;
+    public int timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            txt.SetActive(true);
+            StartCoroutine("WaitFor");
+        }
+
+    }
+    IEnumerator WaitFor()
+    {
+        yield return new WaitForSeconds(timer);
+        Destroy(txt);
+        Destroy(gameObject);
+    }
+}
