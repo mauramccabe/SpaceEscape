dd107a0315c43829e3b83c7ab05961f5ecbab0e5
Ben <Benverney@yahoo.com>
2020-06-04 22:34:18 -0700

    fixed door sounds

--- Assets/Scripts/FirstPressurePlate.cs
+++ Assets/Scripts/FirstPressurePlate.cs
@@ -6,14 +6,25 @@ public class FirstPressurePlate : MonoBehaviour
 {
     [SerializeField]
     GameObject door;
-
+    int pressure = 0;
+    bool hasPlayed = false;
     void OnTriggerEnter(Collider col)
     {
-        door.transform.position = door.transform.position + new Vector3(0, 5, 0);
-        soundManager.PlaySound("plate");
+        door.transform.position = door.transform.position + new Vector3(0, 5, 0); 
+        pressure++;
     }
     void OnTriggerExit(Collider col)
     {
         door.transform.position = door.transform.position + new Vector3(0, -5, 0);
+        pressure--;
+    }
+    void Update() {
+        if(pressure > 1 && !hasPlayed) {
+            soundManager.PlaySound("plate");
+            hasPlayed = true;
+        }
+        if( pressure < 1) {
+            hasPlayed = false;
+        }
     }
 }
